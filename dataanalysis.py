9a41e3dd5a422bf2e49b8984099f27eb008a2c80
kavikatherisan <kavicse195@gmail.com>
2023-08-13 18:42:37 +0530

    documentation

---
+++ dataanalysis.py
@@ -0,0 +1,9 @@
+import logging
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
+log = logging.basicConfig(filename='Analysis.log',format='%(filename)s:%(message)s',level='logging.Debug')
